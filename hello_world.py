26cf7c3b9db6b2579e48bb820978fcdc819808bf
johnspiel99 <njuejohnspiel@gmail.com>
2020-04-07 02:50:15 -0700

    debug.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -6,3 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0', port='5000')
